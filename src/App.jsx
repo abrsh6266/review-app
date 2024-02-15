d8d44ece9f0968985ab4f13c8869d02426810a24
Abrham <abrsh6266@gmail.com>
2024-02-15 04:07:52 -0900

    grabbing first person

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { FaBeer } from "react-icons/fa";
+import people from "./data";
+import { useState } from "react";
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, text, image } = people[index];
+  return (
+    <div>
+      <h2>Reviews Starter</h2>;
+      <FaBeer className="beer"/>
+    </div>
+  );
 };
 export default App;
